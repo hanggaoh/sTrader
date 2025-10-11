9495d0c6c814140d7a244c0a9623ce3dac19cbd2
hanggaoh <hanggaoh@gmail.com>
2025-10-12 02:37:50 +1300

    back fill helper script

--- backup_database.sh
+++ backup_database.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 # This script creates a compressed backup of the production database.
 
-# --- Script Configuration ---
-# set -e: Exit immediately if a command exits with a non-zero status.
-# set -o pipefail: The return value of a pipeline is the status of the last command
-#                  to exit with a non-zero status, or zero if no command exited
-#                  with a non-zero status.
 # Exit immediately if a command exits with a non-zero status.
 set -e
 
